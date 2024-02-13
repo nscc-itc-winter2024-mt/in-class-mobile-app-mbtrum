55165ed8d5ecc76ef0e5b9a6fb502a8f41987006
Michael Trumbull <98288443+mbtrum@users.noreply.github.com>
2024-02-13 18:27:01 +0000

    Started hooking up app to wordpress site.

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default function App() {
+
+  // Url for get all posts
+  //https://nscc-0304263-wordpress-photos.azurewebsites.net/wp-json/wp/v2/posts?_fields=id,title,link,_links&_embed=author,wp:featuredmedia
+
+  const host = 'https://nscc-0304263-wordpress-photos.azurewebsites.net/';
+  const [posts, setPosts] = useState(null);
+
+  // Retrieve posts from Api
+  const getPosts = async () => {
+    const postsUrl = host + 'wp-json/wp/v2/posts?_fields=id,title,link,_links&_embed=author,wp:featuredmedia';
+    const response = await fetch(postsUrl);
+    const posts = await response.json();
+    return posts;
+  }
+
+  // useEffect to load the posts
+  useEffect(() => {
+      getPosts().then(posts => {
+        setPosts(posts);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {posts && posts.map(post => (
+          <>
+            <Image source={{uri: host + post._embedded['wp:featuredmedia'][0].source_url}} style={{width: 400, height: 400}}></Image>
+            <Text key={post.id}>{post.title.rendered}</Text>
+            <Text>{post._embedded.author[0].name}</Text>
+          </>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginBottom: 50,
+    padding: 5
   },
 });
