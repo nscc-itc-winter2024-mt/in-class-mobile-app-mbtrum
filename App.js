2a9245671de6176c14071cc986d66558c9af195a
Michael Trumbull <98288443+mbtrum@users.noreply.github.com>
2024-02-15 14:25:20 +0000

    Adding WordPress integration (troubleshooting bug).

--- App.js
+++ App.js
@@ -1,49 +1,177 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, TextInput, ActivityIndicator } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import base64 from 'react-native-base64';
 
 export default function App() {
 
-  // Url for get all posts
-  //https://nscc-0304263-wordpress-photos.azurewebsites.net/wp-json/wp/v2/posts?_fields=id,title,link,_links&_embed=author,wp:featuredmedia
+  const [photoAsset, setPhotoAsset] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const host = 'https://nscc-0304263-wordpress-photos.azurewebsites.net/';
-  const [posts, setPosts] = useState(null);
+  const host = 'https://nscc-0304263-wordpress-photos.azurewebsites.net';
+  const username = 'W0304263';
+  const apiPassword = 'eWZG kY62 OTzs pMqf rZZ2 GMxM';
 
-  // Retrieve posts from Api
-  const getPosts = async () => {
-    const postsUrl = host + 'wp-json/wp/v2/posts?_fields=id,title,link,_links&_embed=author,wp:featuredmedia';
-    const response = await fetch(postsUrl);
-    const posts = await response.json();
-    return posts;
+  // Choose an image from the camera roll
+  const pickPhoto = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1,1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhotoAsset(result.assets[0]);
+    }
   }
 
-  // useEffect to load the posts
-  useEffect(() => {
-      getPosts().then(posts => {
-        setPosts(posts);
-      });
-  }, []);
+  // Upload image to WordPress
+  const uploadImage = async () => {
+    const endPoint = host + '/wp-json/wp/v2/media';
+    const fileName = photoAsset.uri.split('/').pop();
+    const formData = new FormData();
 
+    formData.append('file', { 
+      uri: photoAsset.uri,
+      name: fileName
+    });
+
+    const result = await fetch(endPoint, {
+      method: 'POST',
+      headers: {        
+        'Content-disposition': 'formdata; filename=' + fileName,
+        'Authorization': 'Basic ' + base64.encode(username + ':' + apiPassword)
+      },
+      body: formData
+    });
+    console.log(result);
+    const response = await result.json();
+    console.log(response);
+    const mediaId = response.id;
+    
+    return mediaId;
+  }
+
+  // Create the Post in WordPress
+  const createPost = async () => {
+
+    if(!title || !photoAsset) {
+      alert('Please complete all fields and choose an image.');
+    }
+    else {
+      // Show spinner
+      //setIsLoading(true);
+
+      // Upload media and get the Id
+      const mediaId = await uploadImage();
+
+
+      //
+      // Create new Post in WordPress
+      //
+      /*const endPoint = host + '/wp-json/wp/v2/posts';
+
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('status', 'publish');
+      formData.append('featured_media', mediaId);
+
+      // WordPress Api call
+      const result = await fetch(endPoint, {
+        method: 'POST',
+        headers: {        
+          'Authorization': 'Basic ' + base64.encode(username + ':' + apiPassword)
+        },
+        body: formData
+      });
+
+      const response = await result.json();
+
+      if(response.id){
+        alert('Your post was sucessfully created. Post Id: ' + response.id);
+      }
+      else{
+        alert('Opps, something went wrong.');
+      }
+
+      setIsLoading(false);*/
+    }
+
+
+  }
+
+
+  // Return the UI
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        {posts && posts.map(post => (
-          <>
-            <Image source={{uri: host + post._embedded['wp:featuredmedia'][0].source_url}} style={{width: 400, height: 400}}></Image>
-            <Text key={post.id}>{post.title.rendered}</Text>
-            <Text>{post._embedded.author[0].name}</Text>
-          </>
-        ))}
-      </ScrollView>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+
+        <Pressable style={styles.button} onPress={pickPhoto}>
+          <Text style={styles.buttonText}>Choose Image</Text>
+        </Pressable>
+
+        { photoAsset && <Image source={{ uri: photoAsset.uri }} style={styles.imageNewPost}></Image>}
+
+        <TextInput
+          style={ styles.textInput }
+          placeholder='Post Title'
+          onChangeText={ text => setTitle(text)}></TextInput>
+
+        <Pressable style={styles.buttonSubmit} onPress={createPost}>
+          <Text style={styles.buttonText}>Create Post</Text>
+        </Pressable>
+
+        { isLoading && <ActivityIndicator></ActivityIndicator> }
+
+      </View>
+    </ScrollView>    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
-    marginBottom: 50,
-    padding: 5
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 50
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'slateblue',
+    marginBottom: 10,
+  },
+  buttonSubmit: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'teal',
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  imageNewPost: {
+    width: 200,
+    height: 200,
+    marginBottom: 10
+  },
+  textInput: {
+    height: 40,
+    marginBottom: 10,
+    borderWidth: 1,
+    padding: 10,
+    width: 300,
+  }
 });
